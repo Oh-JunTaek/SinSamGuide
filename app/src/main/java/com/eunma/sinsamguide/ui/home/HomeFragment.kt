04b84cfe77fba5e7c2f9092caba4bbb98989eedf
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-12-27 00:57:00 +0900

    버튼 기능 활성화 - 이벤트

--- app/src/main/java/com/eunma/sinsamguide/ui/home/HomeFragment.kt
+++ app/src/main/java/com/eunma/sinsamguide/ui/home/HomeFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.eunma.sinsamguide.databinding.DialogEventBinding
 import com.eunma.sinsamguide.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private val homeViewModel: HomeViewModel by viewModels()
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -22,17 +26,48 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val eventButton: Button = binding.buttonEvent
+        eventButton.setOnClickListener {
+            showEventDialog()
+        }
+
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
+    }
+
+    private fun showEventDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("이벤트 선택")
+
+        val dialogBinding: DialogEventBinding = DialogEventBinding.inflate(layoutInflater)
+        val buttonMain: Button = dialogBinding.buttonMain
+        val buttonSeason: Button = dialogBinding.buttonSeason
+        val buttonSpecial: Button = dialogBinding.buttonSpecial
+        buttonMain.setOnClickListener {
+            // 메인 버튼 클릭시 할 일
+        }
+        buttonSeason.setOnClickListener {
+            // 시즌 버튼 클릭시 할 일
+        }
+        buttonSpecial.setOnClickListener {
+            // 특별 버튼 클릭시 할 일
+        }
+
+        builder.setView(dialogBinding.root)
+        builder.setNegativeButton("취소") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.show()
     }
 
     override fun onDestroyView() {
