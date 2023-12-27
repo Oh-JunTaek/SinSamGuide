6236ba3ce89e288dd23c9fcc25fb2dc040ae5a11
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-12-27 19:30:58 +0900

    게스트 로그인

--- app/src/main/java/com/eunma/sinsamguide/LoginActivity.kt
+++ app/src/main/java/com/eunma/sinsamguide/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.eunma.sinsamguide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.eunma.sinsamguide.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -17,7 +19,19 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.guestSignInButton.setOnClickListener {
-            // 게스트 로그인 기능 실행
+            AlertDialog.Builder(this)
+                .setTitle("게스트 로그인")
+                .setMessage("게스트 로그인 시 알림 및 게시판 등의 기능을 사용할 수 없습니다. 계속 진행하시겠습니까?")
+                .setPositiveButton("네") { _, _ ->
+                    moveToMainActivity()
+                }
+                .setNegativeButton("아니오", null)
+                .show()
         }
+    }
+    fun moveToMainActivity() {
+        // 로그인 후 메인 액티비티로 이동
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
