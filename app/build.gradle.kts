ee5b7251e237644381d9bd46dd6a6e6d20a52915
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-12-27 18:12:41 +0900

    파이어베이스 연동

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -53,4 +54,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation ("androidx.fragment:fragment-ktx:1.6.2")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-messaging:23.4.0")
+
 }
