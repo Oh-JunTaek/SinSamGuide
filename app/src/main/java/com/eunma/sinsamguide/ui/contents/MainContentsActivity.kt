b66558667317f1de00d37d7b1a244a3992b59f35
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-12-30 21:35:27 +0900

    메인컨텐츠 - 카드뷰 목록화 완성

--- app/src/main/java/com/eunma/sinsamguide/ui/contents/MainContentsActivity.kt
+++ app/src/main/java/com/eunma/sinsamguide/ui/contents/MainContentsActivity.kt
@@ -2,7 +2,7 @@ package com.eunma.sinsamguide.ui.contents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.eunma.sinsamguide.R
 import com.eunma.sinsamguide.databinding.ActivityMainContentsBinding
 
@@ -14,13 +14,16 @@ class MainContentsActivity : AppCompatActivity() {
         binding = ActivityMainContentsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val gridLayoutManager = GridLayoutManager(this, 2)
+        binding.myRecyclerView.layoutManager = gridLayoutManager
+        binding.myRecyclerView.addItemDecoration(GridSpacingItemDecoration(2, 16))
+
         val dataList: List<ContentsData> = listOf(
             ContentsData(R.drawable.anmoon, "안문 전투"),
             ContentsData(R.drawable.namman, "남만 침입"),
             ContentsData(R.drawable.janngan, "장안 쟁탈전")
         )
 
-        binding.myRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.myRecyclerView.adapter = MyRecyclerViewAdapter(dataList)
     }
 }
--- app/src/main/java/com/eunma/sinsamguide/ui/contents/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/eunma/sinsamguide/ui/contents/MyRecyclerViewAdapter.kt
@@ -1,6 +1,5 @@
 package com.eunma.sinsamguide.ui.contents
 
-import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
