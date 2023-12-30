4f09fa74d5174fb95ca3f2385155f7e13501c486
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-12-30 22:03:18 +0900

    시즌컨텐츠 - 카드뷰 목록화 완성

---
+++ app/src/main/java/com/eunma/sinsamguide/ui/contents/SeasonContentsActivity.kt
@@ -0,0 +1,30 @@
+package com.eunma.sinsamguide.ui.contents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.eunma.sinsamguide.R
+import com.eunma.sinsamguide.databinding.ActivityMainContentsBinding
+import com.eunma.sinsamguide.databinding.ActivitySeasonContentsBinding
+
+class SeasonContentsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySeasonContentsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySeasonContentsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val gridLayoutManager = GridLayoutManager(this, 2)
+        binding.seasonRecyclerView.layoutManager = gridLayoutManager
+        binding.seasonRecyclerView.addItemDecoration(GridSpacingItemDecoration(2, 16))
+
+        val dataList: List<ContentsData> = listOf(
+            ContentsData(R.drawable.baekma, "백마연진 지전"),
+            ContentsData(R.drawable.red_cliffs, "적벽 대전"),
+            ContentsData(R.drawable.hanjung, "한중 전쟁")
+        )
+
+        binding.seasonRecyclerView.adapter = MyRecyclerViewAdapter(dataList)
+    }
+}
