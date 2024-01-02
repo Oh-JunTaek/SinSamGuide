880ada77d12a2269ae7dc9ff5fa927682e213731
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2024-01-02 19:29:03 +0900

    이벤트 카테고리 초안 - 사진 및 설명추가필요

---
+++ app/src/main/java/com/eunma/sinsamguide/ui/event/MainEventsActivity.kt
@@ -0,0 +1,32 @@
+package com.eunma.sinsamguide.ui.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.eunma.sinsamguide.R
+import com.eunma.sinsamguide.databinding.ActivityMainEventsBinding
+import com.eunma.sinsamguide.ui.contents.ContentsData
+import com.eunma.sinsamguide.ui.contents.GridSpacingItemDecoration
+import com.eunma.sinsamguide.ui.contents.MyRecyclerViewAdapter
+
+class MainEventsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainEventsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainEventsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val gridLayoutManager = GridLayoutManager(this, 2)
+        binding.maineventsRecyclerView.layoutManager = gridLayoutManager
+        binding.maineventsRecyclerView.addItemDecoration(GridSpacingItemDecoration(2, 16))
+
+        val dataList: List<ContentsData> = listOf(
+            ContentsData(R.drawable.baekma, "백마연진 지전"),
+            ContentsData(R.drawable.red_cliffs, "적벽 대전"),
+            ContentsData(R.drawable.hanjung, "한중 전쟁")
+        )
+
+        binding.maineventsRecyclerView.adapter = MyRecyclerViewAdapter(dataList)
+    }
+}
---
+++ app/src/main/java/com/eunma/sinsamguide/ui/event/SpecialEventsActivity.kt
@@ -0,0 +1,32 @@
+package com.eunma.sinsamguide.ui.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.eunma.sinsamguide.R
+import com.eunma.sinsamguide.databinding.ActivitySpecialEventsBinding
+import com.eunma.sinsamguide.ui.contents.ContentsData
+import com.eunma.sinsamguide.ui.contents.GridSpacingItemDecoration
+import com.eunma.sinsamguide.ui.contents.MyRecyclerViewAdapter
+
+class SpecialEventsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySpecialEventsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySpecialEventsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val gridLayoutManager = GridLayoutManager(this, 2)
+        binding.specialeventsRecyclerView.layoutManager = gridLayoutManager
+        binding.specialeventsRecyclerView.addItemDecoration(GridSpacingItemDecoration(2, 16))
+
+        val dataList: List<ContentsData> = listOf(
+            ContentsData(R.drawable.baekma, "백마연진 지전"),
+            ContentsData(R.drawable.red_cliffs, "적벽 대전"),
+            ContentsData(R.drawable.hanjung, "한중 전쟁")
+        )
+
+        binding.specialeventsRecyclerView.adapter = MyRecyclerViewAdapter(dataList)
+    }
+}
