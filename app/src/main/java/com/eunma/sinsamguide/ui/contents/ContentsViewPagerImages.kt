ed48a8cd6c415eeb8c3a4c6958fc456037ef2959
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2024-01-03 18:45:26 +0900

    enum class를 활용한 이미지리스트 작성 틀

---
+++ app/src/main/java/com/eunma/sinsamguide/ui/contents/ContentsViewPagerImages.kt
@@ -0,0 +1,10 @@
+package com.eunma.sinsamguide.ui.contents
+
+import com.eunma.sinsamguide.R
+
+enum class ContentsViewPagerImages (val imageResId: Int) {
+    ANMOON(R.drawable.anmoon),
+    ANMOON1(R.drawable.anmoon),
+    ANMOON2(R.drawable.anmoon),
+    // ... 추가로 필요한 이미지들
+}
