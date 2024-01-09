7a9f77279b18c72cff9aaf2ff7343b76cf2b65a4
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2024-01-09 20:49:33 +0900

    이미지 상세보기에서 더 많은 이미지 표현 - 안문

--- app/src/main/java/com/eunma/sinsamguide/ui/contents/MainContentsActivity.kt
+++ app/src/main/java/com/eunma/sinsamguide/ui/contents/MainContentsActivity.kt
@@ -20,7 +20,12 @@ class MainContentsActivity : AppCompatActivity() {
         binding.myRecyclerView.addItemDecoration(GridSpacingItemDecoration(2, 16))
 
         val dataList: List<ContentsData> = listOf(
-            ContentsData(R.drawable.anmoon, "안문 전투 - 군웅토동",getString(R.string.detail_anmoon), listOf()),
+            ContentsData(
+                R.drawable.anmoon,
+                "안문 전투 - 군웅토동",
+                getString(R.string.detail_anmoon),
+                ContentsViewPagerImages.values().filter { it.name.startsWith("ANMOON") }.map { it.imageResId }
+            ),
             ContentsData(R.drawable.namman, "남만 침입",getString(R.string.detail_namman), listOf()),
             ContentsData(R.drawable.janngan, "장안 쟁탈전",getString(R.string.detail_jangan), listOf()),
             ContentsData(R.drawable.gundan, "군단 전투",getString(R.string.detail_gundan), listOf())
